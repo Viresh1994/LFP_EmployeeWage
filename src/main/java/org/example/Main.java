c091d34205de117c4f09d56237e434a1df09a153
Viresh1994 <vireshanadakatni@gmail.com>
2022-11-17 23:03:48 +0530

    ADDED: Code For Employee Part Time Wage

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,20 @@ public class Main {
     public static void main(String[] args) {
         int Wage_Per_Hour = 20;
         int Full_Day_Hour = 8;
+        int Part_Time_Hour = 4;
         int is_Present = 1;
+        int is_Part_Time_Present = 2;
         int Total_Wage;
-        double attendance = Math.floor(Math.random()*10)%2;
+        double attendance = Math.floor(Math.random()*3);
         if(is_Present==attendance) {
             System.out.println("Employee is present");
             Total_Wage = Wage_Per_Hour * Full_Day_Hour;
-        }else{
+        }
+        else if(is_Part_Time_Present==attendance){
+            System.out.println("Employee is present time only");
+            Total_Wage = Wage_Per_Hour * Part_Time_Hour;
+        }
+        else{
             System.out.println("Employee is not present");
             Total_Wage = Wage_Per_Hour * 0;
         }
