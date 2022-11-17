55799766c90b0aa7026770b9281ae57bda7a2b21
Viresh1994 <vireshanadakatni@gmail.com>
2022-11-17 23:12:36 +0530

    MODIFIED: Employee Wage Calculation Using Switch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,18 +8,21 @@ public class Main {
         int is_Present = 1;
         int is_Part_Time_Present = 2;
         int Total_Wage;
-        double attendance = Math.floor(Math.random()*3);
-        if(is_Present==attendance) {
-            System.out.println("Employee is present");
-            Total_Wage = Wage_Per_Hour * Full_Day_Hour;
-        }
-        else if(is_Part_Time_Present==attendance){
-            System.out.println("Employee is present time only");
-            Total_Wage = Wage_Per_Hour * Part_Time_Hour;
-        }
-        else{
-            System.out.println("Employee is not present");
-            Total_Wage = Wage_Per_Hour * 0;
+        int attendance = (int) Math.floor(Math.random()*3);
+
+        switch (attendance){
+            case 1:
+                System.out.println("Employee is present");
+                Total_Wage = Wage_Per_Hour * Full_Day_Hour;
+                break;
+            case 2:
+                System.out.println("Employee is present time only");
+                Total_Wage = Wage_Per_Hour * Part_Time_Hour;
+                break;
+            default:
+                System.out.println("Employee is not present");
+                Total_Wage = Wage_Per_Hour * 0;
+                break;
         }
         System.out.println("Total wage is: " +Total_Wage);
     }
