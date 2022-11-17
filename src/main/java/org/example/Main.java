3ebdb3ccbff0bc99b4a84f6eb0ed70d45912dc0b
Viresh1994 <vireshanadakatni@gmail.com>
2022-11-17 22:54:47 +0530

    ADDED: Code For Employee Daily Wage Calculation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,13 +2,18 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee!");
+        int Wage_Per_Hour = 20;
+        int Full_Day_Hour = 8;
         int is_Present = 1;
+        int Total_Wage;
         double attendance = Math.floor(Math.random()*10)%2;
         if(is_Present==attendance) {
             System.out.println("Employee is present");
+            Total_Wage = Wage_Per_Hour * Full_Day_Hour;
         }else{
             System.out.println("Employee is not present");
+            Total_Wage = Wage_Per_Hour * 0;
         }
+        System.out.println("Total wage is: " +Total_Wage);
     }
 }
