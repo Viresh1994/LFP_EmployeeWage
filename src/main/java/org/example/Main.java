7d08b8e9f0b4d07eccb8e15091d734bde1306540
Viresh1994 <vireshanadakatni@gmail.com>
2022-11-17 22:41:41 +0530

    ADDED: code for to check employee attendence

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Emplyee!");
+        System.out.println("Welcome to Employee!");
+        int is_Present = 1;
+        double attendance = Math.floor(Math.random()*10)%2;
+        if(is_Present==attendance) {
+            System.out.println("Employee is present");
+        }else{
+            System.out.println("Employee is not present");
+        }
     }
 }
