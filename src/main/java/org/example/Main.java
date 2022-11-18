d161e2e4e60ac0b712c6d31203bf846eb70eba47
Viresh1994 <vireshanadakatni@gmail.com>
2022-11-18 08:01:24 +0530

    ADDED: Code For Employee Wage Caclulation Per Month

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,22 +8,28 @@ public class Main {
         int is_Present = 1;
         int is_Part_Time_Present = 2;
         int Total_Wage;
+        int working_Day_Month = 20;
+        int Total_Wage_Month=0;
         int attendance = (int) Math.floor(Math.random()*3);
 
         switch (attendance){
             case 1:
                 System.out.println("Employee is present");
                 Total_Wage = Wage_Per_Hour * Full_Day_Hour;
+                Total_Wage_Month = Total_Wage * working_Day_Month;
+
                 break;
             case 2:
                 System.out.println("Employee is present time only");
                 Total_Wage = Wage_Per_Hour * Part_Time_Hour;
+                Total_Wage_Month = Total_Wage * working_Day_Month;
                 break;
             default:
                 System.out.println("Employee is not present");
                 Total_Wage = Wage_Per_Hour * 0;
                 break;
         }
-        System.out.println("Total wage is: " +Total_Wage);
+        System.out.println("Total daily wage is: " +Total_Wage);
+        System.out.println("Total monthly wage is: " +Total_Wage_Month);
     }
 }
